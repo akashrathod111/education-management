96f4743183d81bf7970bd22157d1e585463d2734
Akash Rathod <rathod.akash111@gmail.com>
2024-02-29 19:13:30 +0530

    add the admin condition for teacher create

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -18,11 +18,16 @@
                 <span class="menu-title">Subject</span>
             </a>
         </li>
+        @php
+            $checkUserType = Auth::user()->user_type;
+        @endphp
+        @if ($checkUserType == 0)    
         <li class="nav-item">
             <a class="nav-link" href="{{url('/teacher')}}">
                 <i class="icon-grid menu-icon"></i>
                 <span class="menu-title">Teacher</span>
             </a>
         </li>
+        @endif
     </ul>
 </nav>
